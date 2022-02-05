9aeb4b9b8ac4569acfd98dd6b224eaa8469013e9
Anthony Koch <adkoch@student.fullsail.edu>
2022-02-04 19:19:42 -0500

    Initial Commit

---
+++ CostumeClosetSwift/CostumeClosetSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostumeClosetSwiftApp.swift
+//  CostumeClosetSwift
+//
+//  Created by Anthony Koch on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CostumeClosetSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
