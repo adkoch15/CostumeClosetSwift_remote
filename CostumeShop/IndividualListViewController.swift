19b82484c723998ce447d7b4e6348595fb68760b
Anthony Koch <adkoch@student.fullsail.edu>
2022-02-06 22:35:48 -0500

    Recreated Individual List View and a storyboard.  Working to complete table integration.

--- CostumeShop/IndividualListViewController.swift
+++ CostumeShop/IndividualListViewController.swift
@@ -12,50 +12,38 @@ class IndividualListViewController: UIViewController, UITableViewDelegate, UITab
     @IBOutlet var table: UITableView!
     @IBOutlet var label: UILabel!
     
-    var models: [(title:String, note:String)] = []
-    
-    override func viewDidLoad() {
+    var models: [(title: String, note: String)] = []
+    override func viewDidLoad(){
         super.viewDidLoad()
-        table.delegate = self
-        table.dataSource = self
-        title = "My Collection"
+        table.delegate = self;
+        table.dataSource = self;
+        title = "Notes"
     }
     
-    @IBAction func didTapNewItem() {
+    @IBAction func didTapNewNote() {
         
     }
     
     //Table
     
-    func tableView(tableView:UITableView, numberofRowsInSection section:Int) -> Int{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
-    
-    func tableView(tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier "cell", for indexPath)
-        cell.textLabel?.text = models[indexPath.row].title
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text=models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].title
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
         //Show note controller
         
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "note") as? NoteViewController else {
-            vc.title = "Note"
-            navigationController?.pushViewController(vc, animated: true)
-        }
-        <#code#>
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {return}
+        vc.title = "Note"
+        navigationController?.pushViewController(vc, animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
