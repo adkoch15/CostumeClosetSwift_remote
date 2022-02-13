e5ff7539c04333991a675331973ade1490e90e53
Anthony Koch <adkoch@student.fullsail.edu>
2022-02-12 23:48:59 -0500

    Added NoteList View to work on list collection, following https://www.youtube.com/watch?v=z48rVDlbyG8&t=34s

---
+++ CostumeShop/NoteList.swift
@@ -0,0 +1,20 @@
+//
+//  NoteList.swift
+//  CostumeShop
+//
+//  Created by Anthony Koch on 2/12/22.
+//
+
+import SwiftUI
+
+struct NoteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteList_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteList()
+    }
+}
